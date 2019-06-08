00ab29e68a82dedf6ba053f2293734beb7f0c3d8
manimalaA <46889094+manimalaA@users.noreply.github.com>
2019-06-08 16:42:46 +0530

    Add files via upload
    This is Calculator program

---
+++ calculator.php
@@ -0,0 +1,19 @@
+
+<?php 
+echo "Enter two numbers";
+$x=readline();
+$y=readline();
+
+echo "choose option:";
+echo "add \n";
+echo "sub \n";
+$e=readline();
+switch($e){
+case "add":
+echo $x+$y;
+break;
+case "sub":
+echo $x-$y;
+break;
+}
+?>
